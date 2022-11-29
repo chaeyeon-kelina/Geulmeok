f154344d92459c0562c77dc79127540ae392a19a
chaeyeon <321codus@naver.com>
2022-11-29 19:35:26 +0900

    Add recommend Activity

--- app/src/main/java/com/example/geulmeok/FirstFragment.kt
+++ app/src/main/java/com/example/geulmeok/FirstFragment.kt
@@ -1,32 +1,79 @@
 package com.example.geulmeok
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.example.geulmeok.databinding.FragmentFirstBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+//    private var _binding: FragmentFirstBinding? = null
+//    private val binding get() = _binding!!
+//    lateinit var binding: FragmentFirstBinding
+
+
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+//        val fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        val fab: View = view.findViewById<FloatingActionButton>(R.id.fab_routine)
+
+        fab.setOnClickListener(({
+            val intent = Intent(context, Recommend::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }))
+        return view;
+//        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+////        initView()
+//        return binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+//        return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+//    private fun initView()= with(binding) {
+////        binding.view.findViewById<FloatingActionButton>(R.id.fab_routine).setOnClickListener {
+//        binding.fabRoutine.setOnClickListener {
+//            Navigation.findNavController(binding.root)
+//                .navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+//    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<FloatingActionButton>(R.id.find_routine).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
+//        Navigation.findNavController(binding.root)
+//        navController = Navigation.findNavController(view)
+
+//        binding.fabRoutine.setOnClickListener {
+//            Navigation.findNavController(binding.root)
+//                .navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+
+//
+//        view.findViewById<FloatingActionButton>(R.id.fab_routine).setOnClickListener {
+//            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 }
--- app/src/main/java/com/example/geulmeok/MainActivity.kt
+++ app/src/main/java/com/example/geulmeok/MainActivity.kt
@@ -1,24 +1,108 @@
 package com.example.geulmeok
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+//import com.example.geulmeok.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tab1 : FirstFragment
+    lateinit var tab2 : chat
+    lateinit var tab3 : timer
+    lateinit var tab4 : chart
+    lateinit var tab5 : profile
+//    lateinit var navController: NavController
+//    companion object{
+//        const val TAG = "MainActivity"
+//    }
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+////
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        val view = binding.root
+//        setContentView(view)
+//        binding..setupWithNavController(navController)
+//        navController = nav_host_fragment.findNavController()
+//
+//        findViewById<FloatingActionButton>(R.id.fab_routine).setOnClickListener {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.view_fragment_page, SecondFragment())
+//                .addToBackStack(null).commit()
+//        }
 
-        findViewById<FloatingActionButton>(R.id.fab_routine).setOnClickListener {
+
+        tab1 = FirstFragment()
+        tab2 = chat()
+        tab3 = timer()
+        tab4 = chart()
+        tab5 = profile()
+
+        supportFragmentManager.beginTransaction().add(R.id.view_page, tab1).commit();
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position){
+                    0 -> {
+                        //Tab1
+                        replaceView(tab1)
+                    }
+                    1 -> replaceView(tab2)
+                    2 -> replaceView(tab3)
+                    3 -> replaceView(tab4)
+                    4 -> replaceView(tab5)
+                }
+            }
+        })
+
+//        val adapter = PagerAdapter(supportFragmentManager)
+////
+//        adapter.addFragment(FirstFragment(), "1번")
+////        adapter.addFragment(chat(), "2번")
+////        adapter.addFragment(timer(), "3번")
+////
+//        viewpager.adapter = adapter
+//        bottom_menu.setupWithViewPager(viewpager)
+////
+//        val adapter = PagerAdapter(supportFragmentManager)
+//        adapter.addFragment(FirstFragment(), "메인")
+//        adapter.addFragment(SecondFragment(), "채팅")
+//        view_fragment_page.adapter = adapter
+//        nav_btn_main.setupWithViewPager(after_login_viewpager)
+//
+
+    }
+
+    private fun replaceView(tab: Fragment){
+        //change fragment view
+        var selectedFragment: Fragment? = null
+        selectedFragment = tab
+        selectedFragment?.let {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.main_page, SecondFragment())
-                .addToBackStack(null).commit()
+                .replace(R.id.view_page, it).commit()
         }
     }
 
